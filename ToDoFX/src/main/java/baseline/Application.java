c9dd3d591fea4dc240cbb8eac3f179098fc137ac
mooree5917 <mooree5917@Knights.ucf.edu>
2021-10-24 23:00:40 -0400

    initial GUI design

--- ToDoFX/src/main/java/baseline/Application.java
+++ ToDoFX/src/main/java/baseline/Application.java
@@ -4,5 +4,30 @@ Copyright 2021 Eric Moore
  */
 package baseline;
 
-public class Application {
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Application extends javafx.application.Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Application.fxml"));
+
+        Scene scene = new Scene(root);
+        /*
+        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+
+        stage.setTitle("JavaFX and Gradle");
+        */
+        stage.setScene(scene);
+
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
