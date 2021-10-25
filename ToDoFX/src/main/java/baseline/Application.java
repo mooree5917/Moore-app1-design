b9ce4f5d868d76a625939dc1377fb861358377fb
mooree5917 <mooree5917@Knights.ucf.edu>
2021-10-24 23:55:47 -0400

    program skeleton

--- ToDoFX/src/main/java/baseline/Application.java
+++ ToDoFX/src/main/java/baseline/Application.java
@@ -11,7 +11,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Application extends javafx.application.Application {
-
+//might need a method or short section of code to ask the user if they would like to load a list.
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("Application.fxml"));
@@ -26,6 +26,37 @@ public class Application extends javafx.application.Application {
 
         stage.show();
     }
+
+    void ToDoTitle(){
+        //To have a user enter the name of their list or use in the main class.
+    }
+
+    public int ToDoListCounter() {
+        //This is a method used to store the number of unique items entered by the user up to a value of 256.
+        return 0;
+    }
+
+    void ToDoDescription(){
+        //Request the user to enter what the item is or its description.
+    }
+
+    void ToDoGoal(){
+        //request the user to complete their list by a date in the form of YYYY/MM/DD
+    }
+
+    void EditToDo(){
+        //To add, remove a new list, or change the title.
+    }
+
+     void ToDoDisplay(){
+        //To display the user's list of item that are complete, incomplete, or existing.
+    }
+
+    void ToDoSave(){
+        //To save the user's list locally.
+    }
+
+
 
     public static void main(String[] args) {
         launch(args);
